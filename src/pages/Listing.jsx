81b48136e402333d4e3933a3b17f6f4131343590
McTyler Tong <mctylert245@gmail.com>
2022-06-14 13:41:57 -0500

    11th edit

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -1,5 +1,9 @@
 import {useState, useEffect} from 'react'
 import {Link, useNavigate, useParams} from 'react-router-dom'
+import { MapContaine, MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+import SwiperCore, {Navigation, Pagination, Scrollbar, A11y} from 'swiper'
+import {Swiper, SwiperSlide} from 'swiper/react'
+import 'swiper/css'
 import {getDoc, doc} from 'firebase/firestore'
 import {getAuth} from 'firebase/auth'
 import {db} from '../firebase.congif'
@@ -36,7 +40,19 @@ function Listing() {
 
     return (
         <main>
-            {/* Slider */}
+            <Swiper slidesPerView={1} pagination={{ clickable: true }}>
+                {listing.imgUrls.map((url, index) => (
+                    <SwiperSlide key={index}>
+                        <div
+                        style={{
+                            background: `url(${listing.imgUrls[index]}) center no-repeat`,
+                            backgroundSize: 'cover',
+                        }}
+                        className='swiperSlideDiv'
+                        ></div>
+                    </SwiperSlide>
+                ))}
+            </Swiper>
 
             <div className="shareIconDiv" onClick={() => {
                 navigator.clipboard.writeText(window.location.href)
@@ -85,7 +101,17 @@ function Listing() {
 
                 <p className="listingLocationTitle">Location</p>
 
-                {/* MAP */}
+                <div className="leafletContainer">
+                    <MapContainer style={{height: '100%', width: '100%'}} center={[listing.geolocation.lat, listing.geolocation.lng]} zoom={13} scollWheelZoom={false}>
+                        <TileLayer
+                            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                            url='https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png'
+                        />
+                        <Marker position={[listing.geolocation.lat, listing.geolocation.lng]}>
+                            <Popup>{listing.location}</Popup>
+                        </Marker>
+                    </MapContainer>
+                </div>
 
                 {auth.currentUser?.uid !== listing.userRef && (
                     <Link to={`/contact/${listing.userRef}?listingName=${listing.name}`} className='primaryButton'>
